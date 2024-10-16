b5635d503c461a03ee8180a2b0f88673134bcb55
Ilay Benjamin <ilaypro1222@gmail.com>
2024-10-16 13:02:57 +0300

    Comitted!

--- dist/server.js
+++ dist/server.js
@@ -15,7 +15,7 @@ const port = 9175; // Set your web server port here
 app.use(express.static(path.join(__dirname, '/../public')));
 // Serve the main index page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../public', 'welcome.html')); // Serve the index.html file
+    res.sendFile(path.join(__dirname, '/../public', 'wefflcome.html')); // Serve the index.html file
 });
 // Proxy requests to /view to the external site (hatmaryoav-site.web.app)
 app.get('/home', (req, res) => {
